a68bf37f3345458e7598aa97d263e1e574f54a32
Kohsuke Kawaguchi <kohsuke@java.net>
2006-07-15 00:09:50 +0000

    added code to populate TestEndpoint.
    svn path=/trunk/; revision=35

--- test-harness/src/com/sun/xml/ws/test/model/TestService.java
+++ test-harness/src/com/sun/xml/ws/test/model/TestService.java
@@ -1,13 +1,17 @@
 package com.sun.xml.ws.test.model;
 
-import com.sun.istack.Nullable;
 import com.sun.istack.NotNull;
+import com.sun.istack.Nullable;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.util.List;
-import java.util.Set;
+import java.io.FileFilter;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * A service to be deployed for a test.
@@ -66,11 +70,68 @@ public class TestService {
 
     public final TestDescriptor parent;
 
-    public TestService(TestDescriptor parent, String name, File baseDir, File wsdl) {
+    public TestService(TestDescriptor parent, String name, File baseDir, File wsdl) throws IOException {
         this.parent = parent;
         this.name = name;
         this.wsdl = wsdl;
         this.baseDir = baseDir;
+
+        // search for classes with @WebService
+        findEndpoints(baseDir);
+    }
+
+    /**
+     * Scans the Java source code in the server directory and
+     * find all classes with @WebService. Those are turned into
+     * {@link TestEndpoint}. 
+     */
+    private void findEndpoints(File dir) throws IOException {
+        File[] dirs = dir.listFiles(new FileFilter() {
+            public boolean accept(File child) {
+                return child.isDirectory();
+            }
+        });
+        for (File subdir : dirs) {
+            findEndpoints(subdir);
+        }
+
+        File[] javas = dir.listFiles(new FileFilter() {
+            public boolean accept(File child) {
+                return child.getName().endsWith(".java");
+            }
+        });
+        for (File src : javas) {
+            // parse the Java file, looking for @WebService
+            // this isn't very robust, but works without compiling them
+            // (note that at this point those source files by themselves won't compile)
+            BufferedReader r = new BufferedReader(new FileReader(src));
+            String line;
+
+            String pkg=null;
+            boolean isWebService = false;
+
+            while((line=r.readLine())!=null) {
+                if(line.startsWith("package ")) {
+                    pkg = line.substring(8,line.indexOf(';'));
+                }
+                if(line.contains("@WebService") || line.contains("@javax.jws.WebService"))
+                    isWebService = true;
+            }
+
+            r.close();
+
+            if(isWebService) {
+                // found it!
+
+                String className = src.getName();
+                className = className.substring(0,className.length()-5); // trim off ".java"
+
+                if(pkg!=null)
+                    className = pkg+'.'+className;
+
+                endpoints.add(new TestEndpoint(src.getName(),className));
+            }
+        }
     }
 
     public String toString() {
