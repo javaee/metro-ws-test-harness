a8bb2c5cb6393b1d6457548811711f6c4a42f4b7
Bhakti Mehta <bhaktimehta@java.net>
2006-07-18 18:46:38 +0000

    Initial checkin
    svn path=/trunk/; revision=78

---
+++ test-harness/test/testcases/wsrm/roundtrip/test.js
@@ -0,0 +1,19 @@
+PingService service = new PingService();
+IPing port = service.getWSHttpBindingIPing();
+System.out.println("first message sent");
+PingRequestBodyType p = new ObjectFactory().createPingRequestBodyType();
+PingResponseBodyType pres = null;
+
+for (i = 0; i<3; i++ ) {
+    p.setText(new JAXBElement<String>(new QName("http://tempuri.org/","Text"),String.class,"Hello There! no" + i));
+    p.setSequence(new JAXBElement<String>(new QName("http://tempuri.org/","Sequence"),String.class,"seq! no" + i));
+
+    pres = port.echoString(p);
+    System.out.println("Returned Value" + pres.getEchoStringReturn().getValue());
+    String retStr = pres.getEchoStringReturn().getValue();
+    assertNotNull(retStr);
+
+}
+
+ClientSession session = ClientSession.getSession((BindingProvider)port);
+session.close();
