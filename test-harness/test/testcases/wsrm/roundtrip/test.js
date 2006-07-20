defb79c38f6e957e13f198750a9fe1968991595b
Bhakti Mehta <bhaktimehta@java.net>
2006-07-20 19:54:12 +0000

    bsh cannot handle generics removing for now
    svn path=/trunk/; revision=111

--- test-harness/test/testcases/wsrm/roundtrip/test.js
+++ test-harness/test/testcases/wsrm/roundtrip/test.js
@@ -1,3 +1,5 @@
+
+
 PingService service = new PingService();
 IPing port = service.getWSHttpBindingIPing();
 System.out.println("first message sent");
@@ -5,8 +7,8 @@ PingRequestBodyType p = new ObjectFactory().createPingRequestBodyType();
 PingResponseBodyType pres = null;
 
 for (i = 0; i<3; i++ ) {
-    p.setText(new JAXBElement<String>(new QName("http://tempuri.org/","Text"),String.class,"Hello There! no" + i));
-    p.setSequence(new JAXBElement<String>(new QName("http://tempuri.org/","Sequence"),String.class,"seq! no" + i));
+    p.setText(new JAXBElement(new QName("http://tempuri.org/","Text"),String.class,"Hello There! no" + i));
+    p.setSequence(new JAXBElement(new QName("http://tempuri.org/","Sequence"),String.class,"seq! no" + i));
 
     pres = port.echoString(p);
     System.out.println("Returned Value" + pres.getEchoStringReturn().getValue());
