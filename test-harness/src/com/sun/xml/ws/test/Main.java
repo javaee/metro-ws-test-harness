915839a08fe7f1bdc882bd9dddc38a690661955a
Kohsuke Kawaguchi <kohsuke@java.net>
2007-09-21 22:28:37 +0000

    making this a debug option.
    svn path=/trunk/; revision=365

--- test-harness/src/com/sun/xml/ws/test/Main.java
+++ test-harness/src/com/sun/xml/ws/test/Main.java
@@ -381,7 +381,8 @@ public class Main {
         String harnessHome = System.getProperty("HARNESS_HOME");
         if(harnessHome!=null) {
             File extDir = new File(new File(harnessHome),"ext");
-            System.err.println("Searching extensions in "+extDir);
+            if(debug)
+                System.err.println("Searching extensions in "+extDir);
             if(extDir.exists()) {
                 for( File f : extDir.listFiles(new FileFilter() {
                     public boolean accept(File f) {
